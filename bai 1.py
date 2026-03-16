e1607051ebb995c44ee88a686f0395aff0c46af1
Tran Nhat Minh <minhmine16@gmail.com>
2026-03-16 07:32:46 +0700

    k

--- bai 1.py
+++ bai 1.py
@@ -2,5 +2,5 @@ dai = float(input())
 rong = float(input())
 dien_tich = dai * rong
 chu_vi = (dai + rong) * 2
-print(dien_tich)
-print(chu_vi)
+print("dien tich: ", dien_tich)
+print("chu vi: ", chu_vi)
---
+++ bai 2.py
@@ -0,0 +1,5 @@
+ban_kinh = float(input())
+dien_tich = 3.14  * ban_kinh ** 2
+chu_vi = 2 * 3.14 * ban_kinh
+print("dien tich: ", dien_tich)
+print("chu vi: ", chu_vi)
